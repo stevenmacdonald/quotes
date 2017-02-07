189bb99b8a2d2a3e44429b5c1236f908a847f1da
Steven <steven.macdonald@gmail.com>
2017-02-08 10:47:45 +1100

    and finally remove the comment

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ MongoClient.connect('mongodb://smacd:smacd@ds145669.mlab.com:45669/quotes', (err
 
 //var randomRec = db.getCollection("quotes")
 
-//adding a comment while I check how pushing changes to github works.
-
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
